6fb7cd6176060ea2314303a13d1c77559685291f
csyjun456 <csyjun456@gmail.com>
2017-10-07 01:03:02 +0800

    adapt self envirment:python 3.5.2 tensorflow:1.1.0rc2

--- fully_connected_feed.py
+++ fully_connected_feed.py
@@ -151,7 +151,7 @@ def run_training():
     eval_correct = mnist.evaluation(logits, labels_placeholder)
 
     # Build the summary operation based on the TF collection of Summaries.
-    summary_op = tf.merge_all_summaries()
+    summary_op = tf.summary.merge_all()
 
     # Create a saver for writing training checkpoints.
     saver = tf.train.Saver()
@@ -164,7 +164,7 @@ def run_training():
     sess.run(init)
 
     # Instantiate a SummaryWriter to output summaries and the Graph.
-    summary_writer = tf.train.SummaryWriter(FLAGS.train_dir,
+    summary_writer = tf.summary.FileWriter(FLAGS.train_dir,
                                             graph_def=sess.graph_def)
 
     # And then after everything is built, start the training loop.
--- mnist.py
+++ mnist.py
@@ -101,11 +101,11 @@ def loss(logits, labels):
   batch_size = tf.size(labels)
   labels = tf.expand_dims(labels, 1)
   indices = tf.expand_dims(tf.range(0, batch_size), 1)
-  concated = tf.concat(1, [indices, labels])
+  concated = tf.concat([indices, labels], 1)
   onehot_labels = tf.sparse_to_dense(
-      concated, tf.pack([batch_size, NUM_CLASSES]), 1.0, 0.0)
-  cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits,
-                                                          onehot_labels,
+      concated, tf.stack([batch_size, NUM_CLASSES]), 1.0, 0.0)
+  cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=logits,
+                                                          labels=onehot_labels,
                                                           name='xentropy')
   loss = tf.reduce_mean(cross_entropy, name='xentropy_mean')
   return loss
@@ -129,7 +129,7 @@ def training(loss, learning_rate):
     train_op: The Op for training.
   """
   # Add a scalar summary for the snapshot loss.
-  tf.scalar_summary(loss.op.name, loss)
+  tf.summary.scalar(loss.op.name, loss)
   # Create the gradient descent optimizer with the given learning rate.
   optimizer = tf.train.GradientDescentOptimizer(learning_rate)
   # Create a variable to track the global step.
